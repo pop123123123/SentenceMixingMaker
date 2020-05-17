1b7ed3887d6cc32d42f2e8cf3455d8801a3f8f4f
Louis MOUHAT <louis.mouhat@gmail.com>
2020-05-17 19:22:42 +0900

    add shortcuts to menu, other ui changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QApplication
 
 if __name__ == "__main__":
     app = QApplication([])
