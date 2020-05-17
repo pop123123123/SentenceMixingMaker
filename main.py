304a624e1d194b65030875a5854ef0c1921bc92c
Louis MOUHAT <louis.mouhat@gmail.com>
2020-05-17 20:20:57 +0900

    add basic actions

--- main.py
+++ main.py
@@ -4,7 +4,44 @@ import sys
 
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
+from PySide2.QtWidgets import QAction, QApplication, QFileDialog
+
+window = None
+saved = False
+changed = False
+
+
+def open():
+    fileName = QFileDialog.getOpenFileName(
+        window,
+        window.tr("Open p00p project"),
+        None,
+        window.tr("P00p project (*.p00p);;All Files (*)"),
+    )
+    print("open", fileName)
+
+
+def save():
+    global saved, changed
+    if not saved:
+        save_as()
+        saved = True
+    elif changed:
+        # save
+        changed = False
+
+
+def save_as():
+    global changed
+    fileName = QFileDialog.getSaveFileName(
+        window,
+        window.tr("Save p00p project"),
+        None,
+        window.tr("P00p project (*.p00p);;All Files (*)"),
+    )
+    changed = False
+    print("open", fileName)
+
 
 if __name__ == "__main__":
     app = QApplication([])
@@ -21,6 +58,13 @@ if __name__ == "__main__":
     if not window:
         print(loader.errorString())
         sys.exit(-1)
+
+    open_action = window.findChild(QAction, "actionOpen")
+    open_action.triggered.connect(open)
+    save_as_action = window.findChild(QAction, "actionSave_as")
+    save_as_action.triggered.connect(save_as)
+    save_as_action = window.findChild(QAction, "actionSave")
+    save_as_action.triggered.connect(save)
     window.show()
 
     sys.exit(app.exec_())
