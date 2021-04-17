61b55bd6264527288677f23eacbef14d28a04eac
Nicolas BUSSER <nicolas.busser67@gmail.com>
2021-04-17 19:32:00 +0200

    Add SpinBoxDelegate arrow buttons implementation

--- delegate/SpinBoxDelegate.py
+++ delegate/SpinBoxDelegate.py
@@ -2,6 +2,7 @@ from PySide2 import QtCore, QtWidgets, QtGui
 from model_ui.segment_model import Columns
 
 class SpinBoxDelegate(QtWidgets.QStyledItemDelegate):
+    """This delegates allow a custom display of a spinbox into a tableview"""
     def __init__(self, parent):
         super().__init__(parent)
 
@@ -20,13 +21,83 @@ class SpinBoxDelegate(QtWidgets.QStyledItemDelegate):
         return None
 
     def setEditorData(self, editor, index):
+        """Handles the way editor store new data"""
         editor.setValue(int(index.data()))
 
     def editorEvent(self, event, model, option, index):
-        pass
+        """This function is used to handle the interactions with handmade
+        buttons.
+        """
+
+        # If the event is a click event
+        if event.type() == QtCore.QEvent.MouseButtonPress:
+            # Retrieves the button positions
+            left_arrow_button, right_arrow_button = self.get_left_right_arrow_buttons(option)
+
+            # Retrieves the combo index value from the model index
+            current_combo_value = int(index.data())
+
+            value_changed = False
+            # If left button was clicked, we decrease combo index
+            if left_arrow_button.contains(event.pos()):
+                if current_combo_value > 0:
+                    value_changed = True
+                    current_combo_value = current_combo_value-1
+
+            # If left button was clicked, we increase combo index
+            if right_arrow_button.contains(event.pos()):
+                # TODO: Globally handle right combo limit (for all the project)
+                value_changed = True
+                current_combo_value = current_combo_value+1
+
+            # Updates model and send signal to the view
+            if value_changed:
+                model._set_attribute_from_index(index, current_combo_value)
+                model.dataChanged.emit(index, index, [QtCore.Qt.EditRole])
 
     def updateEditorGeometry(self, editor, option, index):
         editor.setGeometry(option.rect)
+
+    def get_left_arrow_button(self, option):
+        """Creates the left arrow button, depending on the option rect area"""
+        arrow_buttons_size = option.rect.height()//1.5
+        left_arrow_box = QtCore.QRect(
+            option.rect.left()+1,
+            option.rect.top()+(option.rect.height()//4.5),
+            arrow_buttons_size,
+            arrow_buttons_size)
+        return left_arrow_box
+
+    def paint_left_arrow_button(self, painter, option):
+        """Creates the left arrow button et paints it"""
+        left_arrow_box = self.get_left_arrow_button(option)
+        painter.drawRect(left_arrow_box)
+        painter.fillRect(left_arrow_box, QtGui.QColor(255, 255, 255))
+        painter.drawText(left_arrow_box, QtCore.Qt.AlignCenter, "<")
+
+    def get_right_arrow_button(self, option):
+        """Creates the right arrow button, depending on the option rect area"""
+        arrow_buttons_size = option.rect.height()//1.5
+        right_arrow_box = QtCore.QRect(
+            option.rect.right()-arrow_buttons_size,
+            option.rect.top()+(option.rect.height()//4.5),
+            arrow_buttons_size,
+            arrow_buttons_size)
+        return right_arrow_box
+
+    def paint_right_arrow_button(self, painter, option):
+        """Creates the right arrow button et paints it"""
+        right_arrow_box = self.get_right_arrow_button(option)
+        painter.drawRect(right_arrow_box)
+        painter.fillRect(right_arrow_box, QtGui.QColor(255, 255, 255))
+        painter.drawText(right_arrow_box, QtCore.Qt.AlignCenter, ">")
+
+    def get_left_right_arrow_buttons(self, option):
+        return self.get_left_arrow_button(option), self.get_right_arrow_button(option)
+
+    def paint_left_right_arrow_buttons(self, painter, option):
+        self.paint_left_arrow_button(painter, option)
+        self.paint_right_arrow_button(painter, option)
 
     def paint(self, painter, option, index):
         painter.save()
@@ -47,8 +118,14 @@ class SpinBoxDelegate(QtWidgets.QStyledItemDelegate):
         # If the segment have been analyzed: we draw information
         # Else, we proint a blank cell
         if self.is_segment_analyzed(index):
+            painter.setPen(QtGui.QPen(QtGui.QColor(0, 0, 0)))
+
             painter.drawText(option_spinbox.rect, QtCore.Qt.AlignCenter,
                              str(index.data()))
+
+            self.paint_left_right_arrow_buttons(painter, option_spinbox)
+
+            painter.setPen(QtGui.QPen(QtGui.QColor(255, 255, 255)))
 
             # If the segment associated to the spinbox is selected: green line
             if option_spinbox.state & QtWidgets.QStyle.State_Selected:
