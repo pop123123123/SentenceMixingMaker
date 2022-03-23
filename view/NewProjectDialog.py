7f114648ab47d5c8e448322a427110db51c2943c
Nicolas BUSSER <nicolas.busser67@gmail.com>
2022-03-23 17:14:47 +0100

    add CTRL-V shortcut for pasting youtube urls

--- view/NewProjectDialog.py
+++ view/NewProjectDialog.py
@@ -1,29 +1,47 @@
 # This Python file uses the following encoding: utf-8
 import random
-from PySide2 import QtCore, QtWidgets
+from PySide2 import QtCore, QtWidgets, QtGui
 
 from ui.generated.new import Ui_NewProject
-
 
 class NewProjectDialog(QtWidgets.QDialog):
     def __init__(self, parent):
         QtWidgets.QDialog.__init__(self, parent)
         self.ui = Ui_NewProject()
         self.ui.setupUi(self)
-        self.ui.add_button.clicked.connect(self.add_url)
+
+        self.ui.add_button.clicked.connect(self.add_url_btn)
         self.ui.remove_button.clicked.connect(self.remove_url)
         self.ui.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(
             False
         )
 
-    def add_url(self):
-        self.ui.url_list.addItem("https://www.youtube.com/watch?v=_ZZ8oyZUGn8")
+        paste = QtWidgets.QShortcut(QtGui.QKeySequence("Ctrl+V"), self)
+        paste.activated.connect(self.paste_urls)
+
+    def add_url_btn(self):
+        self.add_url()
+
+    @QtCore.Slot()
+    def paste_urls(self):
+        clipboard = QtGui.QClipboard()
+        urls = clipboard.text()
+        for url in urls.split():
+            self.add_url(url)
+
+    def add_url(self, url=None):
+        if url is not None:
+            self.ui.url_list.addItem(url)
+        else:
+            self.ui.url_list.addItem("https://www.youtube.com/watch?v=_ZZ8oyZUGn8")
+
         item = self.ui.url_list.item(self.ui.url_list.count() - 1)
         item.setFlags(item.flags() | QtCore.Qt.ItemIsEditable)
         self.ui.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(
             True
         )
-        self.ui.url_list.editItem(item)
+        if url is None:
+            self.ui.url_list.editItem(item)
 
     def remove_url(self):
         selected_indexes = self.ui.url_list.selectedIndexes()
